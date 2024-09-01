d4facd4d009a5ef6ff3a37b6a2d8333a494dd211
Anwesh43 <anweshthecool0@gmail.com>
2024-09-01 17:09:26 +0530

    adding useStyle hook

--- src/hook.ts
+++ src/hook.ts
@@ -1,4 +1,4 @@
-import {useEffect, useState} from 'react'
+import {useEffect, useState, CSSProperties} from 'react'
 
 export const useAnimatedScale = (scGap : number = 0.01, delay : number = 20) => {
     const [scale, setScale] = useState<number>(0)
@@ -39,5 +39,39 @@ export const useDimension = () => {
     return {
         w, 
         h
+    }
+}
+
+interface StyleProp {
+    parentStyle() : CSSProperties,
+    blockStyle() : CSSProperties  
+
+}
+
+const sinify : (a : number) => number = (scale : number) : number => Math.sin(scale * Math.PI)
+
+export const useStyle = (w : number, h : number, scale : number) : StyleProp => {
+    const size : number = Math.min(w, h) / 10 
+    const position = 'absolute'
+    const background = 'indigo'
+    return {
+        parentStyle() : CSSProperties {
+            return {
+                position,
+                left: `${w / 2}px`,
+                top: `${h / 2}px`,
+                transform: `rotate(${180 * sinify(scale)}deg)`
+            }
+        },
+        blockStyle() : CSSProperties {
+            return {
+                position,
+                left: `0px`,
+                top: `0px`,
+                width: `${size}px`,
+                height: `${size}px`,
+                background,
+            }
+        }
     }
 }
